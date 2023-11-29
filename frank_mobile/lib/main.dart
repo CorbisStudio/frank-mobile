fc1fc7a6bc945fd98c4ad6096297aa657446f3a6
Florcita <125368448+fgaguero@users.noreply.github.com>
2023-11-29 13:37:52 -0300

    feat: add splashscreen

--- frank_mobile/lib/main.dart
+++ frank_mobile/lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:frank_mobile/login/screens/splashscreen_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+import 'package:frank_mobile/config/theme/theme_data.dart';
+import 'package:frank_mobile/config/router/app_router.dart';
+import 'package:frank_mobile/config/network/dio_manager.dart';
+import 'package:frank_mobile/auth/providers/bloc/auth_bloc.dart';
+import 'package:frank_mobile/config/cache/auth_cache_manager.dart';
+import 'package:frank_mobile/auth/domain/services/auth_service.dart';
+
+void main() {
+  runApp(BlocProvider(
+    create: (_) => AuthBloc(
+      AuthService(DioManager.instance),
+      AuthCacheManager(),
+    ),
+    child: const MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       title: 'Frank Mobile',
-      home: SplashScreenPage()
+      theme: lightTheme,
     );
   }
 }
