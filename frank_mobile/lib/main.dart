b2dca3a6de49e767870156a539e1c35716e44fb4
Florencia <125368448+fgaguero@users.noreply.github.com>
2023-12-27 00:41:34 -0300

    feat: add bloc in main

--- frank_mobile/lib/main.dart
+++ frank_mobile/lib/main.dart
@@ -4,18 +4,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frank_mobile/config/theme/theme_data.dart';
 import 'package:frank_mobile/config/router/app_router.dart';
 import 'package:frank_mobile/config/network/dio_manager.dart';
-import 'package:frank_mobile/auth/providers/bloc/auth_bloc.dart';
 import 'package:frank_mobile/config/cache/auth_cache_manager.dart';
 import 'package:frank_mobile/auth/domain/services/auth_service.dart';
+import 'package:frank_mobile/auth/providers/auth_bloc/auth_bloc.dart';
+import 'package:frank_mobile/auth/providers/login_bloc/login_bloc.dart';
 
 void main() {
-  runApp(BlocProvider(
-    create: (_) => AuthBloc(
-      AuthService(DioManager.instance),
-      AuthCacheManager(),
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthBloc(
+            AuthCacheManager(),
+          ),
+        ),
+        BlocProvider(
+          create: (_) => LoginBloc(
+            AuthService(DioManager.instance),
+          ),
+        ),
+      ],
+      child: const MyApp(),
     ),
-    child: const MyApp(),
-  ));
+  );
 }
 
 class MyApp extends StatelessWidget {
