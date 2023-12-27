29411f2272ceef951fda05b88eb7bde5331fb75a
Florencia <125368448+fgaguero@users.noreply.github.com>
2023-12-27 00:39:34 -0300

    chore: barrel file

--- frank_mobile/lib/config/router/pages.dart
+++ frank_mobile/lib/config/router/pages.dart
@@ -1,2 +1,3 @@
+export 'package:frank_mobile/home/screens/home_page.dart';
 export 'package:frank_mobile/auth/screens/login_page.dart';
 export 'package:frank_mobile/auth/screens/splashscreen_page.dart';
