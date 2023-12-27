17e9b76dcd3283e3cdbdf9564d64eadcf28cd929
Florencia <125368448+fgaguero@users.noreply.github.com>
2023-12-27 00:38:33 -0300

    fix: splashscreen

--- frank_mobile/lib/auth/screens/splashscreen_page.dart
+++ frank_mobile/lib/auth/screens/splashscreen_page.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:frank_mobile/home/screens/home_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 import 'package:frank_mobile/config/router/pages.dart';
-import 'package:frank_mobile/auth/providers/bloc/auth_bloc.dart';
+import 'package:frank_mobile/auth/providers/auth_bloc/auth_bloc.dart';
+
 
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
@@ -27,19 +27,19 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
       onInit: () {
         authBloc = context.read<AuthBloc>()..add(AppStarted());
         authStream = authBloc.stream.listen((state) {
-          status = state.status;
+          Future.delayed(const Duration(seconds: 2)).then((_) {
+            switch (state.status) {
+              case AuthStatus.authenticated:
+                context.goNamed(HomePage.name);
+              case AuthStatus.unauthenticated:
+                context.goNamed(LoginPage.name);
+              case AuthStatus.unknown:
+                context.goNamed(LoginPage.name);
+            }
+          });
         });
       },
-      onEnd: () {
-        switch (status) {
-          case AuthStatus.authenticated:
-            context.goNamed(HomePage.name);
-          case AuthStatus.guest:
-            context.goNamed(LoginPage.name);
-          case AuthStatus.unknown:
-            context.goNamed(LoginPage.name);
-        }
-      },
+      onEnd: () { },
       childWidget: SizedBox(
         height: 200,
         width: 200,
